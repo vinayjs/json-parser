434e354ba459536cf71c8ec409b23be32cf5dcee
VINAY CHANDRAN C <vinay.sarvodaya1991@gmail.com>
2022-10-24 08:30:33 +0530

    Delete src directory

--- src/modules/nullParser.js
+++
@@ -1,8 +0,0 @@
-// null parser - returns parsed output for a null input.
-
-export const parseNull = (input) => {
-    let empty = null;
-    if (input === null || input === "null") {
-      return empty;
-    }
-  };
